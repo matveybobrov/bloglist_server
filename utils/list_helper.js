e4948f5b3f0f154dd869afb03434611722158d61
Matvey <matveybobrov01@gmail.com>
2023-07-03 17:30:35 +0300

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,12 @@
 const dummy = () => 1
 
-module.exports = {
-  dummy,
+const totalLikes = (blogs) => {
+  if (blogs.length === 1) return blogs[0].likes
+  if (blogs.length === 0) return 0
+
+  const reducer = (total, blog) => total + blog.likes
+
+  return blogs.reduce(reducer, 0)
 }
+
+module.exports = { dummy, totalLikes }
