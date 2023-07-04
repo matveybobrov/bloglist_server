316f76675e6a7c5211f0c6e8bcc12a5c3109f74b
Matvey <matveybobrov01@gmail.com>
2023-07-04 16:20:38 +0300

    4.8: Blog list tests, step1

---
+++ tests/blog_api.test.js
@@ -0,0 +1,37 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const app = require('../app')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+}, 100000)
+
+describe('fetching all blogs', () => {
+  test('documents are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  }, 100000)
+
+  test('all documents are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  }, 100000)
+
+  test('specific note is in the collection', async () => {
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map((blog) => blog.title)
+    expect(titles).toContain(helper.initialBlogs[0].title)
+  }, 100000)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+}, 100000)
--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,55 +1,5 @@
 const listHelper = require('../utils/list_helper')
-
-const blogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0,
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0,
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0,
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0,
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0,
-  },
-]
+const helper = require('./test_helper')
 
 test('dummy returns one', () => {
   const result = listHelper.dummy([])
@@ -63,12 +13,12 @@ describe('total likes', () => {
   })
 
   test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes([blogs[0]])
+    const result = listHelper.totalLikes([helper.initialBlogs[0]])
     expect(result).toBe(7)
   })
 
   test('of bigger list is calculated right', () => {
-    const result = listHelper.totalLikes(blogs)
+    const result = listHelper.totalLikes(helper.initialBlogs)
     expect(result).toBe(36)
   })
 })
@@ -80,13 +30,13 @@ describe('favourite blog', () => {
   })
 
   test('for one blog returns the blog itself', () => {
-    const result = listHelper.favouriteBlog([blogs[0]])
-    expect(result).toEqual(blogs[0])
+    const result = listHelper.favouriteBlog([helper.initialBlogs[0]])
+    expect(result).toEqual(helper.initialBlogs[0])
   })
 
   test('for bigger array is calculated correctly', () => {
-    const result = listHelper.favouriteBlog(blogs)
-    expect(result).toEqual(blogs[2])
+    const result = listHelper.favouriteBlog(helper.initialBlogs)
+    expect(result).toEqual(helper.initialBlogs[2])
   })
 })
 
@@ -97,7 +47,7 @@ describe('most blogs', () => {
   })
 
   test("for one blog returns it's author and 1 blog", () => {
-    const result = listHelper.mostBlogs([blogs[0]])
+    const result = listHelper.mostBlogs([helper.initialBlogs[0]])
     expect(result).toEqual({
       author: 'Michael Chan',
       blogs: 1,
@@ -105,7 +55,7 @@ describe('most blogs', () => {
   })
 
   test('for bigger array is calculated correctly', () => {
-    const result = listHelper.mostBlogs(blogs)
+    const result = listHelper.mostBlogs(helper.initialBlogs)
     expect(result).toEqual({
       author: 'Robert C. Martin',
       blogs: 3,
@@ -120,7 +70,7 @@ describe('most likes', () => {
   })
 
   test("for one blog returns it's author and likes", () => {
-    const result = listHelper.mostLikes([blogs[0]])
+    const result = listHelper.mostLikes([helper.initialBlogs[0]])
     expect(result).toEqual({
       author: 'Michael Chan',
       likes: 7,
@@ -128,7 +78,7 @@ describe('most likes', () => {
   })
 
   test('for bigger array is calculated correctly', () => {
-    const result = listHelper.mostLikes(blogs)
+    const result = listHelper.mostLikes(helper.initialBlogs)
     expect(result).toEqual({
       author: 'Edsger W. Dijkstra',
       likes: 17,
