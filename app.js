849cfd92b4f57982611bf836c070c2b4ab3a3d56
Matvey <matveybobrov01@gmail.com>
2023-07-05 12:14:38 +0300

    4.18: bloglist expansion, step6

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const middleware = require('./utils/middleware')
 
 const app = express()
 
+const loginRouter = require('./controllers/login')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 
@@ -28,6 +29,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
